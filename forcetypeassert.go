0c43d22dc07a54a6fa12501bb24dde968f74d01a
Hiroki Suezawa <suezawa@gmail.com>
2020-03-06 22:46:00 +0900

    Test Reviewdog

--- forcetypeassert.go
+++ forcetypeassert.go
@@ -21,7 +21,7 @@ var Analyzer = &analysis.Analyzer{
 	},
 }
 
-const Doc = "forcetypeassert is finds type assertions which did forcely such as below"
+const Doc = "forcetypeassert is finds type assertions which did forcely such as belo"
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
